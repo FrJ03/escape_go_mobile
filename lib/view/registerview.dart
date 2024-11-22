6b440704dd03bb7f894a240fd6813efb84fd9773
i12rumon <i12rumon@uco.es>
2024-11-22 10:17:11 +0100

    fix: update register view to pass integrity tests

--- lib/view/registerview.dart
+++ lib/view/registerview.dart
@@ -3,136 +3,157 @@ import 'package:flutter/material.dart';
 import 'widgets/widgets.dart';
 import 'package:escape_go_mobile/controller/registerController.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: RegisterScreen(),
+    theme: ThemeData(
+      fontFamily: 'Roboto',
+    ),
+  ));
+}
 
 class RegisterScreen extends StatelessWidget {
-  final RegisterController controller=RegisterController();
+  final RegisterController controller = RegisterController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Escape Go',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+        title: Text(
+          'Escape Go',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
         backgroundColor: Color(0xFFA2DAF1),
         centerTitle: true,
       ),
-      body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(10.0),
-        color: const Color(0xFFE7E0EC),
-        child:
-
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 30),
-            Center(child:Image.asset('lib/view/assets/logo.png',height: 150)),
-            Center(
-              child:
-              Text('Registrarse',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // Alinea el texto y el campo de entrada a la izquierda dentro del ancho definido
-                mainAxisSize: MainAxisSize.min, // Ajusta la altura de la columna a sus contenidos
-                children: [
-                  Text('Usuario:',style:  TextStyle(fontWeight: FontWeight.bold,fontSize: 17)),
-                  SizedBox(height: 8),
-                  SizedBox(
-                    width: 300.0, // Cambia este valor para ajustar el ancho
-                    child: TextField(
-                      controller: controller.nameController,
-                      decoration: InputDecoration(
-                        hintText: 'Introduzca el nombre de usuario',
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(10.0),
+          color: const Color(0xFFE7E0EC),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              Center(child: Image.asset('lib/view/assets/logo.png', height: 150)),
+              Center(
+                child: Text(
+                  'Registrarse',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Usuario:',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                    SizedBox(height: 8),
+                    SizedBox(
+                      width: 300.0,
+                      child: TextField(
+                        controller: controller.nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Introduzca el nombre de usuario',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height:16),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // Alinea el texto y el campo de entrada a la izquierda dentro del ancho definido
-                mainAxisSize: MainAxisSize.min, // Ajusta la altura de la columna a sus contenidos
-                children: [
-                  Text('Email:',style:  TextStyle(fontWeight: FontWeight.bold,fontSize: 17)),
-                  SizedBox(height: 8),
-                  SizedBox(
-                    width: 300.0, // Cambia este valor para ajustar el ancho
-                    child: TextField(
-                      controller: controller.emailController,
-                      decoration: InputDecoration(
-                        hintText: 'Introduzca el correo electrónico',
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+              SizedBox(height: 16),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Email:',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                    SizedBox(height: 8),
+                    SizedBox(
+                      width: 300.0,
+                      child: TextField(
+                        controller: controller.emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Introduzca el correo electrónico',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // Alinea el texto y el campo de entrada a la izquierda dentro del ancho definido
-                mainAxisSize: MainAxisSize.min, // Ajusta la altura de la columna a sus contenidos
-                children: [
-                  Text('Contraseña:',style:  TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),
-                  SizedBox(height: 8),
-                  SizedBox(
-                    width: 300.0, // Cambia este valor para ajustar el ancho
-                    child: TextField(
-                      controller: controller.passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Introduzca la contraseña',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
-
+              SizedBox(height: 16),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Contraseña:',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                    SizedBox(height: 8),
+                    SizedBox(
+                      width: 300.0,
+                      child: TextField(
+                        controller: controller.passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Introduzca la contraseña',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-
-            SizedBox(height: 100),
-            Center(
-              child:
-              CustomButton(
-                value: 'Registrarse',
-                color: Color(0xFFA2DAF1),
-                onPressed: () {
-                  controller.register(context);
-                  controller.nameController.text='';
-                  controller.passwordController.text='';
-                  controller.emailController.text='';
-
-
-                }
+              SizedBox(height: 100),
+              Center(
+                child: CustomButton(
+                  key: Key('register_button'),
+                  value: 'Registrarse',
+                  color: Color(0xFFA2DAF1),
+                  onPressed: () {
+                    controller.register(context);
+                    controller.nameController.text = '';
+                    controller.passwordController.text = '';
+                    controller.emailController.text = '';
+                  },
+                ),
               ),
-            ),
-            Center(
-                child:
-                Column(
-                  children:
-                  [
-                    Text('¿Tienes cuenta?',style: TextStyle(fontSize: 17),),
-                    InkWell(child:
-                    Text('Iniciar sesión',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,decoration: TextDecoration.underline)),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      '¿Tienes cuenta?',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    InkWell(
+                      child: Text(
+                        'Iniciar sesión',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),
+                      ),
                       onTap: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(builder: (context) => LoginScreen()),
                         );
                       },
-                    )
-
+                    ),
                   ],
-                )
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
