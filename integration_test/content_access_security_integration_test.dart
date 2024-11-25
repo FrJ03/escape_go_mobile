c7e1ea792192f08eec0340493cec382fae422ae5
i12telol <118206793+i12telol@users.noreply.github.com>
2024-11-25 20:26:24 +0100

    fix: fixed indentation

--- integration_test/content_access_security_integration_test.dart
+++ integration_test/content_access_security_integration_test.dart
@@ -4,7 +4,7 @@ import 'package:escape_go_mobile/view/loginview.dart' as app;
 import 'package:flutter/material.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  	IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
 	testWidgets('Prueba de seguridad de acceso a contenido (sin introducir credenciales)', (WidgetTester tester) async {
 		// Inicia la aplicación
@@ -12,22 +12,22 @@ void main() {
 		await tester.pumpAndSettle();
 
 		final loginButton = find.byKey(Key('login_button'));
-    await tester.tap(loginButton);
+    		await tester.tap(loginButton);
 
 		await tester.pumpAndSettle();
 
 		final errorDialog = find.text('Error');
-    expect(errorDialog, findsOneWidget);
+    		expect(errorDialog, findsOneWidget);
 
 		final okButton = find.widgetWithTest(TextButton, 'OK');
 		await tester.tap(okButton);
-    await tester.pumpAndSettle();
+    		await tester.pumpAndSettle();
 
 		final page = find.text('Iniciar sesión')
 		expect(page, findsOneWidget);
 		
 		// sigue en la página de login, acceso restringido
-  });
+  	});
 
 	testWidgets('Prueba de seguridad de acceso a contenido (introduciendo credenciales de usuario no registrado)', (WidgetTester tester) async {
 		// Inicia la aplicación
@@ -36,24 +36,24 @@ void main() {
 
 		// introduce credenciales
 		final userField = find.byType(TextField).at(0);
-    await tester.enterText(userField, 'usuarionoexiste');
-    final passwordField = find.byType(TextField).at(1);
-    await tester.enterText(passwordField, 'noregistrado');
+    		await tester.enterText(userField, 'usuarionoexiste');
+    		final passwordField = find.byType(TextField).at(1);
+    		await tester.enterText(passwordField, 'noregistrado');
 
 		final loginButton = find.byKey(Key('login_button'));
-    await tester.tap(loginButton);
+    		await tester.tap(loginButton);
 
 		await tester.pumpAndSettle();
 
 		final errorDialog = find.text('Error');
-    expect(errorDialog, findsOneWidget);
+    		expect(errorDialog, findsOneWidget);
 
 		final okButton = find.widgetWithTest(TextButton, 'OK');
 		await tester.tap(okButton);
-    await tester.pumpAndSettle();
+    		await tester.pumpAndSettle();
 
 		final page = find.text('Iniciar sesión')
 		expect(page, findsOneWidget);
 		// sigue en la página de login, acceso restringido porque el usuario no está registrado
-  });
+  	});
 }
