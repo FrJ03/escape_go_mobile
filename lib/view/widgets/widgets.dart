99ce94c0c34938d6ec6d9f29554c183d497f6914
i12rumon <i12rumon@uco.es>
2024-11-28 11:53:34 +0100

    fix: create new button to admin panel

--- lib/view/widgets/widgets.dart
+++ lib/view/widgets/widgets.dart
@@ -31,3 +31,38 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
+
+
+
+class PanelButton extends StatelessWidget {
+  final String value;
+  final Color color;
+  final VoidCallback onPressed;
+
+  const PanelButton({
+    Key? key,
+    required this.value,
+    required this.color,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color, // Color de fondo del botón
+          foregroundColor: Colors.black, // Color del texto
+          padding: const EdgeInsets.all(12),
+          fixedSize: const Size(250, 75)
+        ),
+        child: Text(
+          value,
+          style: const TextStyle(fontSize: 17),
+        ),
+      ),
+    );
+  }
+}
