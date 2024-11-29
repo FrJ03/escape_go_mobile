75370e04fc8bf8786a2b9c424a1ba0fc9b1dd15c
i12rumon <i12rumon@uco.es>
2024-11-29 23:01:44 +0100

    fix: add widgets to admin view screen

--- lib/view/widgets/widgets.dart
+++ lib/view/widgets/widgets.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CustomButton extends StatelessWidget {
   final String value;
@@ -63,6 +65,68 @@ class PanelButton extends StatelessWidget {
           style: const TextStyle(fontSize: 17),
         ),
       ),
+    );
+  }
+}
+
+
+class ImagePickerWidget extends StatefulWidget {
+  @override
+  _ImagePickerWidgetState createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+  File? _image;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: _pickImage,
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: _image != null
+                ? ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.file(
+                _image!,
+                fit: BoxFit.cover,
+              ),
+            )
+                : Center(
+              child: Icon(Icons.add_a_photo, color: Colors.grey, size: 50),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            if (_image != null) {
+              print("Imagen seleccionada: ${_image!.path}");
+            } else {
+              print("No se ha seleccionado ninguna imagen");
+            }
+          },
+          child: Text("Subir Imagen"),
+        ),
+      ],
     );
   }
 }
