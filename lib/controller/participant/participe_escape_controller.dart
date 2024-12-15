db22c781d8a4f72d55da78dca0e1fc031888125a
i12rumon <i12rumon@uco.es>
2024-12-15 16:13:34 +0100

    feat: create participate in Escape Room controller

---
+++ lib/controller/participant/participe_escape_controller.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+
+class ParticipateEscController {
+
+  //En un futuro se llamara al servidor para pedir estos datos
+  //Pero mientras utilizaremos estos datos de prueba
+  final String tittle = 'Escapa de la mansión';
+  final String description = '¡Escapa de una escalofriante mansión antes de morir congelado!';
+  final  String level = 'Avanzado';
+  final String solution = 'Solución ';
+  final int price = 245;
+
+
+
+
+   Future <void> participe(BuildContext context) async{
+     _showDialog(context, 'Participar', '¿Estás seguro de que quieres participar?');
+  }
+
+
+
+
+
+  void _showDialog(
+      BuildContext context,
+      String tittle,
+      String message, {
+        VoidCallback? onConfirm, // Callback opcional
+      }) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          tittle,
+          style: TextStyle(color: Color(0xFFFFA1A1), fontWeight: FontWeight.bold),
+        ),
+        content: Text(message),
+        actions: [
+          TextButton(
+              //Si le da a que si se llevará al participante a la pantalla con la pista
+                onPressed: () => Navigator.pop(context),
+                //FocusScope.of(context).unfocus();
+                //Navigator.pushAndRemoveUntil(
+                  //context,
+                  //MaterialPageRoute(builder: (context) => PanelScreen()),
+                      //(route) => false,
+                //);
+
+              child: Text('SÍ', style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            // Solo cierra el diálogo
+            child: Text('NO', style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
