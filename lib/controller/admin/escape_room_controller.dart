294d26895ffaee79420cc86519c7ff3699c3a9bc
Yo <franjomor@gmail.com>
2024-12-15 13:32:23 +0100

    feat: delete escape room controller method

--- lib/controller/admin/escape_room_controller.dart
+++ lib/controller/admin/escape_room_controller.dart
@@ -2,25 +2,35 @@ import 'package:escape_go_mobile/domain/escape_rooms/escape_room.dart';
 import 'package:http/http.dart' as http;
 
 class EscapeRoomController{
-  Future<bool> create_escape_room(EscapeRoom escape_room) async {
+  Future<bool> createEscapeRoom(EscapeRoom escapeRoom) async {
     final Object body = {
-      'title': escape_room.title,
-      'description': escape_room.description,
-      'solution': escape_room.solution,
-      'difficulty': escape_room.difficulty,
-      'price': escape_room.price,
-      'maxSessionDuration': escape_room.maxSessionDuration,
+      'title': escapeRoom.title,
+      'description': escapeRoom.description,
+      'solution': escapeRoom.solution,
+      'difficulty': escapeRoom.difficulty,
+      'price': escapeRoom.price,
+      'maxSessionDuration': escapeRoom.maxSessionDuration,
       'location':{
-        'country': escape_room.location.country,
-        'city': escape_room.location.city,
-        'street': escape_room.location.street,
-        'street_number': escape_room.location.streetNumber,
-        'coordinates': escape_room.location.coordinates,
-        'info': escape_room.location.info
+        'country': escapeRoom.location.country,
+        'city': escapeRoom.location.city,
+        'street': escapeRoom.location.street,
+        'street_number': escapeRoom.location.streetNumber,
+        'coordinates': escapeRoom.location.coordinates,
+        'info': escapeRoom.location.info
       }
     };
     
     final response = await http.post(Uri.parse('http://localhost:3000/escaperoom/create'), body: body);
+
+    if(response.statusCode == 200){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  Future<bool> deleteEscapeRoom(int id) async {
+    final response = await http.delete(Uri.parse('http://localhost:3000/escaperoom/create?id=$id'));
 
     if(response.statusCode == 200){
       return true;
