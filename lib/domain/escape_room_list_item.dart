28a132b67493bc30ccbdde463631ba0caf7bd227
Yo <franjomor@gmail.com>
2024-12-15 08:14:31 +0100

    feat: gets from escape room list item

--- lib/domain/escape_room_list_item.dart
+++ lib/domain/escape_room_list_item.dart
@@ -1,15 +1,15 @@
 import 'location.dart';
 
 class EscapeRoomListItem{
-  final id;
-  final title;
-  final location;
+  final _id;
+  final _title;
+  final _location;
 
-  const EscapeRoomListItem({
-    required this.id,
-    required this.title,
-    required this.location
-  });
+  const EscapeRoomListItem(
+    this._id,
+    this._title,
+    this._location
+  );
 
   factory EscapeRoomListItem.fromJson(Map<String, dynamic> json){
     return switch (json) {
@@ -19,11 +19,15 @@ class EscapeRoomListItem{
         'location': Map<String, dynamic> location
       } =>
         EscapeRoomListItem(
-          id: id,
-          title: title,
-          location: Location.fromJson(location)
+          id,
+          title,
+          Location.fromJson(location)
         ),
       _ => throw const FormatException('Failed to load escape room list item.'),
       };
   }
+
+  int get id => this._id;
+  String get title => this._title;
+  Location get location => this._location;
 }
