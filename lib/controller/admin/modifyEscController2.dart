b688336f164d725c47bc0e527e2a6a4e3c419c5c
i12rumon <i12rumon@uco.es>
2024-12-16 17:29:08 +0100

    feat: create modify escape controller to add clues to a escape room

---
+++ lib/controller/admin/modifyEscController2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class EscapeRoomController {
+  final TextEditingController storyController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  List<Map<String, dynamic>> fragments = [];
+
+  void addFragment(Function showError) {
+    if (storyController.text.isNotEmpty && nameController.text.isNotEmpty ) {
+      fragments.add({
+        'info': storyController.text,
+        'name': nameController.text,
+
+      });
+      storyController.clear();
+      nameController.clear();
+    }
+  }
+}
