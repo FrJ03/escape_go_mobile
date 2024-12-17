e37206e11342a819af0616fb99de3ad551899b89
i12rumon <i12rumon@uco.es>
2024-12-17 10:10:10 +0100

    feat: created statistics controller

---
+++ lib/controller/admin/statisticsController.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatisticsController {
+
+  final int conversion=0;
+  final int session=0;
+  final int  growth=0;
+  //Aquí se realizaría la llamada a la API para obtener estos datos de la base de datos
+
+
+
+
+
+
+}
