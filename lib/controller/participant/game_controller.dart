a72ed7db20933126909ed827c8589254aa82ed0c
i12telol <118206793+i12telol@users.noreply.github.com>
2024-12-29 20:50:45 +0100

    fix: added call solve api

--- lib/controller/participant/game_controller.dart
+++ lib/controller/participant/game_controller.dart
@@ -107,6 +107,54 @@ class GameController{
     }
   }
 
+
+  // Resuelve el escape room
+    Future<int> solve(String solution, int escapeRoomId, int participationId) async {
+      try {
+        // Obtener el token del usuario autenticado
+        final token = await _getToken();
+        if (token == null) {
+          throw Exception('No se encontró un token válido. Inicia sesión nuevamente.');
+        }
+        // ENDPOINT
+        final url = Uri.parse('http://localhost:3000/game/solve');
+        // Cuerpo de la solicitud
+        final Object body = {
+          'solution': solution,
+          'escape_room_id': escapeRoomId,
+  	'participation_id': participationId,
+        };
+        // Encabezados
+        final headers = {
+          'Authorization': token,
+          'Content-Type': 'application/json',
+        };
+        // SOLICITUD POST
+        final response = await http.post(url,headers: headers,body: jsonEncode(body),
+        );
+        // RESPUESTAS
+        switch (response.statusCode) {
+          case 200:
+            // CORRECTO
+            final json = jsonDecode(response.body);
+            return json['points'] as int;
+          case 400:
+            throw Exception('Error en solicitud: ${response.body}');
+          case 401:
+            throw Exception('No eres admin.');
+          case 404:
+            throw Exception('Escape Room no existe.');
+  	case 423:
+            throw Exception('Participacion no iniciada o finalizada.');
+          default:
+            throw Exception('Hubo un error inesperado: ${response.statusCode}');
+        }
+      } catch (e) {
+        throw Exception('Hubo un error: $e');
+      }
+    }
+
+  
   /// Obtener el token almacenado
   Future<String?> _getToken() async {
     final prefs = await SharedPreferences.getInstance();
